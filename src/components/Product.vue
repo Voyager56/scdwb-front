124dfee5d69d942f87f7d2faff005eb47673db9c
Levan Mikatadze <78099948+Voyager56@users.noreply.github.com>
2023-07-06 05:03:46 +0400

    update mass delete

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="border-[#e76f51] py-2 px-3 border mx-5 flex flex-col items-center break-all relative">
-        <input type="checkbox" class="absolute left-2 delete-checkbox"
-            @click="$emit('checked', product.id)"
+    <div class="border-[#e76f51] py-2 px-3 border mx-5 flex flex-col items-center break-all relative w-[15rem]">
+          <input type="checkbox" class="absolute left-2 delete-checkbox"
+            @change="$emit('checked', product.id)"
         />
         <p>{{ product.sku }}</p>
         <p>{{ product.name }}</p>
--- src/pages/ProductList.vue
+++ src/pages/ProductList.vue
@@ -8,8 +8,8 @@
             </div>
         </div>
         <div class="mx-5 my-5 flex">
-            <div v-for="(product, index) in products" class="w-[20rem] flex flex-wrap">
-                <Product :product="product" :key="index" @checked="checkBoxClickedEvent"/>
+            <div v-for="(product, index) in products" class="w-[20rem] flex flex-wrap overflow-auto">
+                <Product v-model="checkedProducts.index" :product="product" :index="index" :key="index" @checked="checkBoxClickedEvent"/>
             </div>
         </div>
     </div>
@@ -20,36 +20,49 @@ import { ref } from 'vue'
 import Product from '@/components/Product.vue'
 
 const products = ref([])
-const checkedProducts = ref([])
+const checkedProducts = ref({})
 
 fetch('https://scandiweb-levan-mikatadze.000webhostapp.com/product')
     .then(res => res.json())
     .then(data => {
-      console.log(data)
         products.value = data
     })
     .catch(err => console.log(err))
 
 
 const checkBoxClickedEvent = (id) => {
-    if(checkedProducts.value.includes(id)){
-        checkedProducts.value = checkedProducts.value.filter(item => item !== id)
+    if(checkedProducts.value[id]){
+        delete checkedProducts.value[id]
     }else{
-    console.log(checkedProducts.value)
-        checkedProducts.value.push(id)
+        checkedProducts.value[id] = id
     }
 }
 
 const massDelete = () => {
-    if(checkedProducts.value.length === 0){
-        console.log("No products selected")
-        return
-    }
-    let form = new FormData()
-    checkedProducts.value.forEach(id => {
-        form.append('ids[]', id)
-    })
-  console.log(checkedProducts.value)
+  if(checkedProducts.value.length === 0){
+      console.log('no products selected')
+      return
+  }
+  let form = new FormData()
+  document.querySelectorAll('.delete-checkbox').forEach(checkbox => {
+      if(checkbox.checked){
+        const index = checkbox.parentNode.getAttribute('index')
+        const productId = products.value[index].id
+        form.append(`ids[]`, productId)
+      }
+  })
+
+  // if you want to use v-model instead of querySelectorAll but its not working in tests
+  // selecting elements and manually setting checked property isn't working with vue
+
+  //   let form = new FormData()
+  //   // transform object to array
+  //    let productsArray = Object.values(checkedProducts.value)
+  //    productsArray.forEach(productId => {
+  //         form.append(`ids[]`, productId)
+  //    })
+
+
     fetch('https://scandiweb-levan-mikatadze.000webhostapp.com/product/massDelete', {
         method: 'POST',
         body: form
